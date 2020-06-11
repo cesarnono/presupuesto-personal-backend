bd64eb2fe92703371f0c8ae06f20893bd7bcdb9f
CEIBA\cesar.aguirre <cesar.aguirre@ceiba.com.co>
2020-06-11 17:15:27 -0500

    configuracion cors

--- src/main/java/com/gestion/cuentas/configuracion/Config.java
+++ src/main/java/com/gestion/cuentas/configuracion/Config.java
@@ -11,7 +11,8 @@ public class Config implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedHeaders("*").exposedHeaders("authorization","Authorization");
     }
 }
 
